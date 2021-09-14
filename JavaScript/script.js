abd7dbf74f206b02e3260ebfb92413b0dc7c7edb
taichir0923 <crystal19950923@gmail.com>
2021-09-14 18:15:51 +0800

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -226,3 +226,234 @@
 // } else {
 //     console.log(x + ": Палиндром тоо биш...")
 // }
+
+// var x = prompt("Эхний тоог оруул");
+// var y = prompt("2 дахь тоог оруул");
+
+// console.log(x ** y)
+// var z = prompt("3 дахь тоог оруул");
+
+// var counter = 0;
+
+// if(+x === 0){
+//     counter++;
+// }
+// if(+y === 0){
+//     counter++;
+// }
+// if(+z === 0){
+//     counter++;
+// }
+
+// for(var i = 0; i <= 100; i++){
+//     if(i >= 10 && i < 100){
+//         console.log(i)
+//     }
+// }
+
+// var x = prompt('too oruul'); // 123
+// // console.log(x.length);
+// var counter = 0;
+// for(var i = 0; i < x.length; i++){
+//     if(x[i]%3 === 0){
+//         counter++; // counter = counter + 1
+//     }
+// }
+
+// crystal19950923@gmail.com
+
+// Loop , var;
+
+// Loop - Davtalt
+
+// nuhtsol - true, false
+
+// 1 - true, 0 - false
+
+// for(var i = 1; i < 10; i++){
+//     if(i%3 === 0){ // === 1, 0, true
+//         console.log(i)
+//     }
+// }
+
+// Array [5, 23, 6, 5, 2, 3]
+
+// var email = prompt('Email haygaa oruulna uu...')
+// var password = prompt('Password')
+
+// if(email === "narada@mail.com"){
+//     if(password === "myPass1234"){
+//         console.log('Тавтай морил')
+//     } else {
+//         console.log("Имэйл эсвэл нууц үг буруу байна.")
+//     }
+// } else {
+//     console.log('Хэрэглэгч олдсонгүй')
+// }
+
+
+// index
+// var ner = "Narada";
+// //         012345
+
+// for(var i = 0; i < ner.length; i++){
+//     console.log(ner[i]);
+// }
+
+// Arrays - Data structure
+// array - Массив
+
+// for(var i = 0; i < array.length; i++){
+//     if(typeof array[i] === 'string'){
+//         console.log(array[i])
+//     }
+// }
+
+// for(el of array){
+    //     if(typeof el === 'number'){
+        //         console.log(el)
+        //     }
+        // }
+        
+        // "Narada"
+        // "N" - Nice
+        // "a" - awesome
+        // "r" - reasonable
+        // "a" - amazing
+        // "d" - dancer
+        // "a" - attractive
+                
+// var array = ["narada", 23, true, "John", "Sarah", 456];
+
+// var random = parseInt(Math.random() * array.length);
+
+// console.log(array[random]);
+
+// 1. Ooriin neriig oruul.
+// 2. Neriin useg bolgon dr 5 ug hadgal
+// 3. nernii useg bolgoniig ymar useg bolohiig shalga
+// 4. tuhain usgeer ehlesen ugnuudees sanamsargui bdlaar ng ug songo
+// 5. ur dung console-d hevle
+
+// [5, 6, 0, 1, 2, 7]   target = 9;
+
+// // Array methods
+// var arr = [5, 6, 7, 8, 9];
+
+// // push - массивийн сүүлд элемент нэмж өгнө
+// arr.push(10, 11, 12, 13, 14);
+
+// // pop - массивийн сүүлийн элементийг устгана
+// arr.pop();
+// arr.pop();
+// arr.pop();
+
+// // unshift - массивийн эхэнд элемент нэмнэ
+// arr.unshift(2, 3, 4);
+
+// // shift() - массивийн эхний элементийг устгана
+// arr.shift();
+
+// // reverse() - урвуу массив үүсгэнэ.
+// arr.reverse();
+
+// arr.pop()
+// // 11, 10, 9, 8, 7, 6, 5, 4, 3
+// // splice - массив дотроос дурын элементийг устгана
+// arr.splice(3, 1)
+
+// // 11, 10, 9, 7, 6, 5, 4, 3
+// // slice - arr-g tasdah
+
+// var x = arr.slice(1, 4);
+
+// ES1, 2, 3 - EcmaScript
+// ES5 - 2009 - Buh Browser demjine / var /
+// ES6 - 2015 - shine standart  / let /
+// ES7 - 2016
+// ES8 - 2017
+// ES9 - 2018
+// ES10 - 2019
+// ES11 - 2020
+// ES12 - 2021
+
+// Оролт: [5, 6, 0, 1, 2, 7]   target = 9;
+// i = 0 uyd0 indextei element 5 bna.
+// 5+6, 5+0, 5+1, 5+2, 5+7;
+
+// i = 1 uyd 1 indextei element 6 bna
+// 6+0, 6+1, 6+2, 6+7;
+
+// i = 2 => 0
+
+
+// Гаралт: [4, 5]
+
+// let arr = [5, 6, 0, 1, 2, 7];
+// let target = 9;
+
+// let result = [];
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = i + 1; j < arr.length; j++){
+//         if(arr[i] + arr[j] === target){
+//             result.push(i, j);
+//         }
+//     }
+// }
+
+// console.log(result)
+
+// let arr = ["Ulaanbaatar", 123, true, "Beijing", 258, "Tokyo", false, "Seoul"];
+
+// let result = [];
+
+// for(let i = 0; i < arr.length; i++){
+//     if(typeof arr[i] === "string"){
+//         result.push(arr[i])
+//     }
+// }
+
+// Array - Data Structure
+// Object
+
+let user = {
+    ner: "Narada",
+    email: "narada@mail.com",
+    mergejil: "Designer"
+}
+
+user.ner = "Jessica";
+user.utas = 99663322;
+
+let user1 = {
+    ner: "John",
+    email: "john@exapmle.com",
+    mergejil: "Teacher"
+}
+// [{}, {}, {}, {}]
+// let users = [{ner: "Gantulga", nas: 25}, {ner: "Nyamgerel"}]
+
+// Array
+let email = prompt("Haih email oruul");
+let users = [user, user1];
+
+// for(let i = 0; i < users.length; i++){
+    // if(users[i].email === email){
+    //     console.log(users[i]);
+    // }
+// }
+
+// for in, for of
+
+// for(usr of users){
+//     if(usr.email === email){
+//         console.log(usr);
+//     }
+// }
+
+// for(pro in user){
+//     if(pro === "email"){
+//         console.log(user[pro])
+//     }
+// }
