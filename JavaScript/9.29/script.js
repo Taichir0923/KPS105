14bcee2b6ccdb3773014ce5b0bea45914ac98c6a
taichir0923 <crystal19950923@gmail.com>
2021-09-30 17:16:25 +0800

    Update script.js

--- JavaScript/9.29/script.js
+++ JavaScript/9.29/script.js
@@ -5,7 +5,32 @@ const input = document.querySelector('#input');
 const nums = document.querySelectorAll('.num')
 
 function number(number) {
-    input.value += number;
+    if (input.value[input.value.length - 1] === number && (number === '+' || number === '-' || number === '*' || number === '/' || number === '.')) {
+        input.value = input.value.slice(0, -1);
+        input.value += number
+    } else if(
+        (number === '+' && (
+        input.value[input.value.length - 1] === '-' ||
+        input.value[input.value.length - 1] === '*' ||
+        input.value[input.value.length - 1] === '/' )) ||
+        (number === '-' && (
+        input.value[input.value.length - 1] === '+' ||
+        input.value[input.value.length - 1] === '*' ||
+        input.value[input.value.length - 1] === '/' )) ||
+        (number === '*' && (
+        input.value[input.value.length - 1] === '-' ||
+        input.value[input.value.length - 1] === '+' ||
+        input.value[input.value.length - 1] === '/' )) ||
+        (number === '/' && (
+        input.value[input.value.length - 1] === '-' ||
+        input.value[input.value.length - 1] === '*' ||
+        input.value[input.value.length - 1] === '+' ))
+    ){
+        input.value = input.value.slice(0, -1);
+        input.value += number
+    } else {
+        input.value += number;
+    }
 }
 
 function calculate() {
@@ -24,7 +49,7 @@ for (let i = 0; i < nums.length; i++) {
             number(nums[i].innerText)
         }
     } else {
-        nums[i].onclick = function(){
+        nums[i].onclick = function () {
             calculate()
         }
     }
