8f5fc3a122e91732baa862992da5210f825345d7
taichir0923 <crystal19950923@gmail.com>
2021-09-30 18:27:14 +0800

    123

--- JavaScript/9.29/script.js
+++ JavaScript/9.29/script.js
@@ -3,8 +3,10 @@
 // } // ES6
 const input = document.querySelector('#input');
 const nums = document.querySelectorAll('.num');
+const sanah = document.querySelector('.sana');
 
 let isPressed = false;
+let memorize = 0;
 
 function number(number) {
     if (input.value[input.value.length - 1] === number && (number === '+' || number === '-' || number === '*' || number === '/' || number === '.')) {
@@ -30,6 +32,10 @@ function number(number) {
     ){
         input.value = input.value.slice(0, -1);
         input.value += number
+    } else if(number === '<='){
+        input.value = input.value.slice(0, -1);
+    } else if(number === 'AC'){
+        input.value = ''
     } else {
         if(!isPressed){
             input.value += number;
@@ -65,6 +71,21 @@ for (let i = 0; i < nums.length; i++) {
         nums[i].onclick = function () {
             calculate();
             isPressed = true;
+        }
+    }
+}
+
+sanah.onclick = function(){
+    if(isPressed){
+        memorize = input.value;
+        input.value = '';
+        isPressed = false
+    } else {
+        if(memorize !== 0){
+            input.value += memorize;
+            memorize = 0
+        } else {
+            alert('too hadgalaagui bna')
         }
     }
 }
