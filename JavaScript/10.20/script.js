a062078bd00f134d88a4aa758c8c745b9fbaaeea
taichir0923 <crystal19950923@gmail.com>
2021-10-21 17:53:55 +0800

    Update script.js

--- JavaScript/10.20/script.js
+++ JavaScript/10.20/script.js
@@ -43,40 +43,40 @@ var array = [52, 15, 45, 63, 87, 12, 52, 0];
 // // [15, 52]     [45, 63]    [12, 87]    [0, 52]
 // //    [15, 45, 52, 63]        [0, 12, 52, 87]
 // //        [0, 12, 15, 45, 52, 52, 63, 87]
-// function merArray(left, right){
-//     var result = [];
-//     var i = 0;
-//     var j = 0;
+function merArray(left, right){
+    var result = [];
+    var i = 0;
+    var j = 0;
 
-//     while(left.length > i && right.length > j){
-//         if(left[i] > right[j]){
-//             result.push(right[j]);
-//             j++;
-//         } else {
-//             result.push(left[i]);
-//             i++;
-//         }
-//     }
+    while(left.length > i && right.length > j){
+        if(left[i] > right[j]){
+            result.push(right[j]);
+            j++;
+        } else {
+            result.push(left[i]);
+            i++;
+        }
+    }
 
-//     for(i; i < left.length; i++){
-//         result.push(left[i]);
-//     }
+    for(i; i < left.length; i++){
+        result.push(left[i]);
+    }
 
-//     for(j; j < right.length; j++){
-//         result.push(right[j]);
-//     }
+    for(j; j < right.length; j++){
+        result.push(right[j]);
+    }
 
-//     return result;
-// }
+    return result;
+}
 
-// function mergeSort(arr){
-//     if(arr.length <= 1) return arr;
-//     var midIndex = Math.floor(arr.length / 2);
-//     var left = mergeSort(arr.slice(0, midIndex));
-//     var right = mergeSort(arr.slice(midIndex));
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    var midIndex = Math.floor(arr.length / 2);
+    var left = mergeSort(arr.slice(0, midIndex));
+    var right = mergeSort(arr.slice(midIndex));
     
-//     return merArray(left, right);
-// }
+    return merArray(left, right);
+}
 
 // getDigit(number, i) => 5
 // 10 * 0 = 1
